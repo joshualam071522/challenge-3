1679f47a3ebe460b3bf55ef97eb670cbfbe504e9
joshualam071522 <127908020+joshualam071522@users.noreply.github.com>
2023-04-18 23:24:46 -0700

    moved alphabetBlank to inside the function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-var alphabetBlank = []
 var alphabetUpperCase = ['A', 'B', 'C', 'E', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var alphabetLowerCase = ['a', 'b', 'c', 'e', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var alphabetNumeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
