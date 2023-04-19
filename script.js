38ea34fc3bd273ffaf9fbe98e4ded1956b06f430
joshualam071522 <127908020+joshualam071522@users.noreply.github.com>
2023-04-18 18:53:29 -0700

    changed variable names to make script more readable

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Assignment code here
 function generatePassword() {
 
-  //Password Character Length
-  var characterLength = prompt("How Many characters in your password?", 8);
-  if (characterLength<8) {
+  //Password Character Conditions
+  var passwordLength = prompt("How Many characters in your password?", 8);
+  if (passwordLength<8) {
     alert("Must be at least 8 characters");
     return;
-  } else if (characterLength >= 8 && characterLength <= 128) {
-  var characterUpperCase = confirm("Click OK to include upper case characters");
-  var characterLowerCase = confirm("Click OK to include lower case characters");
-  var characterNumeric = confirm("Click OK to include numeric characters");
-  var characterSpecial = confirm("Click OK to include special characters");
+  } else if (passwordLength >= 8 && passwordLength <= 128) {
+  var passwordUpperCase = confirm("Click OK to include upper case characters");
+  var passwordLowerCase = confirm("Click OK to include lower case characters");
+  var passwordNumeric = confirm("Click OK to include numeric characters");
+  var passwordSpecial = confirm("Click OK to include special characters");
   } else {
     alert("Cannot be more than 128 characters");
     return; 
