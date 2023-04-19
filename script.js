8a992a22c12274bedb0823d863c55856201b2070
joshualam071522 <127908020+joshualam071522@users.noreply.github.com>
2023-04-18 18:45:35 -0700

    Added conditions for generated password

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment code here
+function generatePassword() {
 
-
+  //Password Character Length
+  var characterLength = prompt("How Many characters in your password?", 8);
+  if (characterLength<8) {
+    alert("Must be at least 8 characters");
+    return;
+  } else if (characterLength >= 8 && characterLength <= 128) {
+  var characterUpperCase = confirm("Click OK to include upper case characters");
+  var characterLowerCase = confirm("Click OK to include lower case characters");
+  var characterNumeric = confirm("Click OK to include numeric characters");
+  var characterSpecial = confirm("Click OK to include special characters");
+  } else {
+    alert("Cannot be more than 128 characters");
+    return; 
+  }
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
